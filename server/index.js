b1bc6803c13b9ec4a1e727eabe29b179cacb3689
Niraj kumar chaurasiya <116571542+nirajchaurasiya@users.noreply.github.com>
2023-04-11 16:17:14 +0545

    new updates

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
 const corsOptions = {
-    // origin: 'http://localhost:3000'
-    origin: 'https://twitter-6tvv.onrender.com'
+    origin: 'http://localhost:3000'
+    // origin: 'https://twitter-6tvv.onrender.com'
 };
 app.use(cors(corsOptions));
 app.use(function (req, res, next) {
