87cbe531548f1133190014a8ce7a6b93d2596cea
Niraj kumar chaurasiya <116571542+nirajchaurasiya@users.noreply.github.com>
2023-04-11 08:41:39 +0545

    new update

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ app.use(express.static('uploads'))
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 app.use('/api/auth', require('./auth/auth'));
 app.use('/api/user', require('./user/user'));
-
+app.get('/', (req, res) => {
+    res.send({ "msg": "success" })
+})
 app.listen(PORT, () => {
     console.log(`Backend is running on ${PORT}`)
 })
